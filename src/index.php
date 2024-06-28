fcb07ab167ff47851101fcc55e0555dac6f6a450
Fábio <fabio.tanimura@outlook.com>
2024-06-28 04:10:16 -0300

    feature:mysql

--- src/index.php
+++ src/index.php
@@ -1,3 +1,16 @@
 <?php
 echo "Olá, mundo!";
+$servername = "mysql";
+$username = "myuser";
+$password = "mypassword";
+$dbname = "mydatabase";
+
+// Criar conexão
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Verificar conexão
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
 ?>
