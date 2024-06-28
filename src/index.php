ae4bce7abc8c71669cb0cb336daa5741e9511b96
Fabio Tanimura <fabio.tanimura@outlook.com>
2024-06-28 11:33:16 -0300

    fix: Ajustado PDO

--- src/index.php
+++ src/index.php
@@ -4,13 +4,15 @@ $servername = "mysql";
 $username = "myuser";
 $password = "mypassword";
 $dbname = "mydatabase";
+$port = 3307;
 
-// Criar conexão
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Verificar conexão
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+try {
+    // Criar conexão PDO com a porta especificada
+    $conn = new PDO("mysql:host=$servername;port=$port;dbname=$dbname", $username, $password);
+    // Configurar o modo de erro do PDO para exceção
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+} catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
 }
-echo "Connected successfully";
 ?>
